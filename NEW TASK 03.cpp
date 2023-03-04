882c178318619ed4dec2f0adf32f5b89bb5611df
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-03-04 14:37:10 +0200

    NEW TASK 03

---
+++ NEW TASK 03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+//Write a program that prints its own code.
+
+void printCode(const char* name) {
+	std::ifstream file(name);
+	if (!file.is_open()) {
+		cout << "Error";
+		return;
+	}
+	while (!file.eof()) {
+		char buff [1024];
+		file.getline(buff, 1024);
+		cout << buff << endl;
+
+	}
+
+	file.close();
+
+}
+
+int main() {
+	printCode("Task 06.cpp");
+
+}
